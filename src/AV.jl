7dcab0d698692cfe9f04e509a86a19a893947561
Kevin Squire <kevin@secondspectrum.com>
2014-06-06 15:59:04 -0700

    More rename issues.  I should test before pushing.

--- src/AV.jl
+++ src/AV.jl
@@ -1,29 +1,29 @@
 module AV
 
 const lib="libav-0.8"
-w(f) = joinpath(Pkg.dir("libAV"), "src", lib, f)
+w(f) = joinpath(Pkg.dir("AV"), "src", lib, f)
 
 export Codec, SWScale, Format
 include(w("exports.jl"))
 include(w("libav_h_ext.jl"))
 
 module Codec
-  using libAV
-  import libAV.w
+  using AV
+  import AV.w
   include(w("libav_h_ext.jl"))
   include(w("avcodec.jl"))
 end
 
 module SWScale
-  using libAV
-  import libAV.w
+  using AV
+  import AV.w
   include(w("libav_h_ext.jl"))
   include(w("swscale.jl"))
 end
 
 module Format
-  using libAV
-  import libAV.w
+  using AV
+  import AV.w
   include(w("libav_h_ext.jl"))
   include(w("avformat.jl"))
   include(w("avio.jl"))
@@ -31,4 +31,4 @@ end
 
 include("av_capture.jl")
 
-end # libAV
+end # AV
