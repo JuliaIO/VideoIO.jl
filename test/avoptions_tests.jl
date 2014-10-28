f4651da3469af71331ff0c8ab9a67d581076f51a
Max <mxsstr93@gmail.com>
2014-10-28 22:52:29 +0100

    AVOptions - remove tests for Linux * due to problems with device autodetection on Travis

--- test/avoptions_tests.jl
+++ test/avoptions_tests.jl
@@ -5,7 +5,7 @@ using VideoIO
 println(STDERR, "Now testing the AVOptions API...")
 
 @linux_only begin
-   println(STDERR, "Test#1: Not possible to detect AVOptions-enabled devices in Linux... skipping")
+   println(STDERR, "Not possible to autodetect AVOptions-enabled devices in Linux... skipping tests")
 end
 
 @osx_only begin
@@ -15,6 +15,52 @@ end
         error("Cannot detect any FFmpeg-enabled input video devices!")
     else
         println("Detected $(length(devices_list.idevice_name)) input video devices... Passed \n")
+    end
+
+
+   println(STDERR, "Test#2: Open default camera device and format...")
+   f = opencamera()
+   if (f.avin==C_NULL)
+       error("Device detection failed, you will have to test the API manually!")
+   else
+      println("Detected default camera $(VideoIO.DEFAULT_CAMERA_DEVICE) and its format... Passed \n")
+#     img = read(f, Image)
+#     canvas, _ = view(img)
+
+#     counter = 250 #frames
+#     while counter > 0
+#         read!(f, img)
+#         view(canvas, img)
+#         counter -=1
+#     end
+   end
+
+
+   if (f.avin.isopen)
+       println("-"^75)
+       println(STDERR, "Test#3: Check whether API options are accessible...")
+       fmt_options, codec_options = document_all_options(f, true)
+       if isempty(fmt_options)
+           error("Cannot access options")
+       else
+           options = collect(keys(fmt_options))
+           println("Detected $(length(options)) options... \n")
+
+           # Select 2 options, test get and set with strings
+           println(STDERR, "Test#4: Now testing get options with strings...")
+           !isempty(get_option(f, options[1]))? println("get ", options[1],"... passed"): error("get failed")
+           !isempty(get_option(f, options[3]))? println("get ", options[3],"... passed"): error("get failed")
+
+           # Select 2 options, set with AVDictionary API
+           println(STDERR, "\nTest#5: Now testing AVDictionary API...")
+           entries = Dict{String,String}()
+           for i=1:4
+               entries[options[i]] = string((fmt_options[options[i]][1]+fmt_options[options[i]][2])/2)
+           end
+           create_dictionary(entries) != C_NULL ? println("Built an AVDictionary... passed \n"): error("Failed")
+       end
+    # Close the camera input
+    close(f)
     end
 end
 
@@ -26,14 +72,13 @@ end
     else
         println("Detected $(length(devices_list.idevice_name)) input video devices... Passed \n")
     end
-end
 
-println(STDERR, "Test#2: Open default camera device and format...")
-f = opencamera()
-if (f.avin==C_NULL)
-    error("Device detection failed, you will have to test the API manually!")
-else
-    println("Detected default camera $(VideoIO.DEFAULT_CAMERA_DEVICE) and its format... Passed \n")
+    println(STDERR, "Test#2: Open default camera device and format...")
+    f = opencamera()
+    if (f.avin==C_NULL)
+       error("Device detection failed, you will have to test the API manually!")
+    else
+      println("Detected default camera $(VideoIO.DEFAULT_CAMERA_DEVICE) and its format... Passed \n")
 #     img = read(f, Image)
 #     canvas, _ = view(img)
 
@@ -43,35 +88,37 @@ else
 #         view(canvas, img)
 #         counter -=1
 #     end
-end
+    end
 
+   if (f.avin.isopen)
+       println("-"^75)
+       println(STDERR, "Test#3: Check whether API options are accessible...")
+       fmt_options, codec_options = document_all_options(f, true)
+       if isempty(fmt_options)
+           error("Cannot access options")
+       else
+           options = collect(keys(fmt_options))
+           println("Detected $(length(options)) options... \n")
 
-if (f.avin.isopen)
-   println("-"^75)
-   println(STDERR, "Test#3: Check whether API options are accessible...")
-   fmt_options, codec_options = document_all_options(f, true)
-   if isempty(fmt_options)
-       error("Cannot access options")
-   else
-       options = collect(keys(fmt_options))
-       println("Detected $(length(options)) options... \n")
+           # Select 2 options, test get and set with strings
+           println(STDERR, "Test#4: Now testing get options with strings...")
+           !isempty(get_option(f, options[1]))? println("get ", options[1],"... passed"): error("get failed")
+           !isempty(get_option(f, options[3]))? println("get ", options[3],"... passed"): error("get failed")
 
-       # Select 2 options, test get and set with strings
-       println(STDERR, "Test#4: Now testing get options with strings...")
-       !isempty(get_option(f, options[1]))? println("get ", options[1],"... passed"): error("get failed")
-       !isempty(get_option(f, options[3]))? println("get ", options[3],"... passed"): error("get failed")
-
-       # Select 2 options, set with AVDictionary API
-       println(STDERR, "\nTest#5: Now testing AVDictionary API...")
-       entries = Dict{String,String}()
-       for i=1:4
-           entries[options[i]] = string((fmt_options[options[i]][1]+fmt_options[options[i]][2])/2)
+           # Select 2 options, set with AVDictionary API
+           println(STDERR, "\nTest#5: Now testing AVDictionary API...")
+           entries = Dict{String,String}()
+           for i=1:4
+               entries[options[i]] = string((fmt_options[options[i]][1]+fmt_options[options[i]][2])/2)
+           end
+           create_dictionary(entries) != C_NULL ? println("Built an AVDictionary... passed \n"): error("Failed")
        end
-       create_dictionary(entries) != C_NULL ? println("Built an AVDictionary... passed \n"): error("Failed")
-   end
-# Close the camera input
-close(f)
+    # Close the camera input
+    close(f)
+    end
 end
+
+
 
 
 
