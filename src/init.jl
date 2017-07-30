4bb5d771f09e118a490a010582343565af23642a
Romain Franconville <romain.franconville@gmail.com>
2017-07-30 11:59:25 -0400

    Removing some file path confusion when @__DIR__ is called through several scripts

--- src/init.jl
+++ src/init.jl
@@ -1,12 +1,12 @@
 # convenient way to get library information
-using BinDeps,Compat
+using BinDeps
 
 # export avcodec_dir, avformat_dir, avutil_dir, swscale_dir, avdevice_dir, avfilter_dir, avresample_dir, swresample_dir,
 #        avcodec_version, avformat_version, avutil_version, swscale_version, avdevice_version, avfilter_version, avresample_version, swresample_version,
 #        libavcodec, libavformat, libavutil, libswscale, libavdevice, libavfilter, libavresample, libswresample,
 
 if !isdefined(:libavcodec)
-    include(joinpath(dirname(@__DIR__),"deps","deps.jl"))
+    include(joinpath("..","deps","deps.jl"))
 end
 
 INSTALL_ROOT = realpath(joinpath(splitdir(libavcodec)[1], ".."))
