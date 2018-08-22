10ca772b610c7255cae5d7873e2e08fad03b1766
Robin Deits <robin.deits@gmail.com>
2018-08-22 00:29:58 -0400

    use Requires so that load order doesn't matter

--- src/VideoIO.jl
+++ src/VideoIO.jl
@@ -2,7 +2,7 @@ __precompile__(false)
 
 module VideoIO
 
-using FixedPointNumbers, ColorTypes, ImageCore
+using FixedPointNumbers, ColorTypes, ImageCore, Requires
 
 include("init.jl")
 include(joinpath(av_load_path, "AVUtil", "src", "AVUtil.jl"))
@@ -20,5 +20,38 @@ include("util.jl")
 include("avio.jl")
 include("testvideos.jl")
 using .TestVideos
+
+function __init__()
+    @require ImageView="86fae568-95e7-573e-a6b2-d8a6b900c9ef" begin
+        # Define read and retrieve for Images
+        function play(f, flip=false)
+            buf = read(f)
+            canvas, _ = ImageView.imshow(buf, flipx=flip, interactive=false)
+
+            while !eof(f)
+                read!(f, buf)
+                ImageView.imshow(canvas, buf, flipx=flip, interactive=false)
+                sleep(1/f.framerate)
+            end
+        end
+
+        function playvideo(video)
+            f = VideoIO.openvideo(video)
+            play(f)
+        end
+
+        if have_avdevice()
+            function viewcam(device=DEFAULT_CAMERA_DEVICE, format=DEFAULT_CAMERA_FORMAT)
+                camera = opencamera(device, format)
+                play(camera, true)
+            end
+        else
+            function viewcam()
+                error("libavdevice not present")
+            end
+        end
+    end
+end
+
 
 end # VideoIO
