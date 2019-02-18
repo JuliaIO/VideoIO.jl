b5240ad10a536a6a4cc4fadc16ca7222cdd1cd5e
Ian Butterworth <i.r.butterworth@gmail.com>
2019-02-18 11:50:45 -0800

    show expected deps path

--- src/init.jl
+++ src/init.jl
@@ -4,6 +4,7 @@ using BinaryProvider
 # Load in `deps.jl`, complaining if it does not exist
 const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
 if !isfile(depsjl_path)
+    println("Deps path: $depsjl_path")
     error("VideoIO not installed properly, run Pkg.build(\"VideoIO\"), restart Julia and try again")
 end
 include(depsjl_path)
