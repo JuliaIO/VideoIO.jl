f58ce5d649c59e90c1f93c81447404d3d59cf045
Ian Butterworth <i.r.butterworth@gmail.com>
2019-04-21 20:16:39 -0400

    Change _get_fc to provide Ptr{AVFormatContext}

--- src/util.jl
+++ src/util.jl
@@ -30,9 +30,9 @@ av_pointer_to_field(s::Array, name::Symbol) = av_pointer_to_field(pointer(s), na
 
 function _get_fc(file::String) # convenience function for `get_duration` and `get_start_time`
     v = open(file)
-    return unsafe_load(v.apFormatContext[1])
+    return v.apFormatContext[1]
 end
-get_duration(fc::AVFormatContext) = Dates.Millisecond(fc.duration) # this is a bit risky: if AV_TIME_BASE ≠ 1e6 then this conversion will give false results, or if `fc.duration` is not a whole number then this will result in an InexactError. I'll add the appropriate checks if you'll tell me that either event or both are possible.
+get_duration(fc::Ptr{AVFormatContext}) = Dates.Millisecond(fc[].duration) # this is a bit risky: if AV_TIME_BASE ≠ 1e6 then this conversion will give false results, or if `fc.duration` is not a whole number then this will result in an InexactError. I'll add the appropriate checks if you'll tell me that either event or both are possible.
 
 """
     get_duration(file::String) -> Millisecond
@@ -41,7 +41,7 @@ Return the duration of the video `file` in `Millisecond`s.
 """
 get_duration(file::String) = get_duration(_get_fc(file))
 
-get_start_time(fc::AVFormatContext) = Dates.unix2datetime(fc.start_time_realtime)
+get_start_time(fc::Ptr{AVFormatContext}) = Dates.unix2datetime(fc[].start_time_realtime)
 
 """
     get_start_time(file::String) -> DateTime
@@ -50,7 +50,7 @@ Return the starting date & time of the video `file`. Note that if the starting d
 """
 get_start_time(file::String) = get_start_time(_get_fc(file))
 
-get_time_duration(fc::AVFormatContext) = (get_start_time(fc), get_duration(fc))
+get_time_duration(fc::Ptr{AVFormatContext}) = (get_start_time(fc), get_duration(fc))
 
 """
     get_time_duration(file::String) -> (DateTime, Millisecond)
