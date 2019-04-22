02f569dc71d7bd98e223394910e39e4058a4b6e8
Ian <i.r.butterworth@gmail.com>
2019-04-22 03:41:48 -0400

    Resorting to BinaryProvider only given linux binary available

--- src/init.jl
+++ src/init.jl
@@ -1,5 +1,16 @@
-if Sys.islinux()
-    include("init-linux.jl")
-else
-    include("init-notlinux.jl")
+## convenient way to get library information
+using BinaryProvider
+
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    println("Deps path: $depsjl_path")
+    error("VideoIO not installed properly, run Pkg.build(\"VideoIO\"), restart Julia and try again")
 end
+include(depsjl_path)
+
+if !@isdefined(ffmpeg_or_libav)
+    include("version.jl")
+end
+
+av_load_path = joinpath(dirname(@__FILE__), ffmpeg_or_libav)
