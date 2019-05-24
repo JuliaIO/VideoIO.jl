41063bfaca92e8c69da4dc84fc7cfe760ae5cc0a
Ian <i.r.butterworth@gmail.com>
2019-05-24 10:38:35 -0400

    conform to ap object names

--- src/encoding.jl
+++ src/encoding.jl
@@ -5,37 +5,37 @@ export encodevideo, encode!, prepareencoder, appendencode!, finishencode!, mux
 # A struct collecting encoder objects for easy passing
 mutable struct VideoEncoder
     codec_name::String
-    pcodeccontext::Array{Ptr{VideoIO.AVCodecContext}}
-    pframe::Array{Ptr{VideoIO.AVFrame}}
-    ppkt::Array{Ptr{VideoIO.AVPacket}}
+    apCodecContext::Array{Ptr{AVCodecContext}}
+    apFrame::Array{Ptr{AVFrame}}
+    apPacket::Array{Ptr{AVPacket}}
     pix_fmt::Integer
 end
 
 """
-encode(enc_ctx::Ptr{VideoIO.AVCodecContext}, pktptr::Ptr{VideoIO.AVPacket}, io::IO)
+encode(encoder::VideoEncoder, io::IO)
 
 Encode frame in memory
 """
 function encode!(encoder::VideoEncoder, io::IO; flush=false)
     if flush
-        ret = avcodec_send_frame(encoder.pcodeccontext[1],C_NULL)
+        ret = avcodec_send_frame(encoder.apCodecContext[1],C_NULL)
     else
-        ret = avcodec_send_frame(encoder.pcodeccontext[1],encoder.pframe[1])
+        ret = avcodec_send_frame(encoder.apCodecContext[1],encoder.apFrame[1])
     end
     ret < 0 && error("Error $ret sending a frame for encoding")
 
     while ret >= 0
-        ret = avcodec_receive_packet(encoder.pcodeccontext[1], encoder.ppkt[1])
+        ret = avcodec_receive_packet(encoder.apCodecContext[1], encoder.apPacket[1])
         if (ret == -35 || ret == -541478725) # -35=EAGAIN -541478725=AVERROR_EOF
              return
         elseif (ret < 0)
             error("Error $ret during encoding")
         end
-        pkt = unsafe_load(encoder.ppkt[1])
+        pkt = unsafe_load(encoder.apPacket[1])
         @debug println("Write packet $(pkt.pts) (size=$(pkt.size))")
         data = unsafe_wrap(Array,pkt.data,pkt.size)
         write(io,data)
-        VideoIO.av_packet_unref(encoder.ppkt[1])
+        av_packet_unref(encoder.apPacket[1])
     end
 end
 
@@ -67,48 +67,48 @@ function prepareencoder(firstimg,codec_name,framerate,AVCodecContextProperties)
     codec = avcodec_find_encoder_by_name(codec_name)
     codec == C_NULL && error("Codec '$codec_name' not found")
 
-    codeccontextptr = Ptr{VideoIO.AVCodecContext}[VideoIO.avcodec_alloc_context3(codec)]
-    codeccontextptr == [C_NULL] && error("Could not allocate video codec context")
+    apCodecContext = Ptr{AVCodecContext}[avcodec_alloc_context3(codec)]
+    apCodecContext == [C_NULL] && error("Could not allocate video codec context")
 
-    pktptr = Ptr{VideoIO.AVPacket}[VideoIO.av_packet_alloc()]
-    pktptr == [C_NULL] && error("av_packet_alloc() error")
+    apPacket = Ptr{AVPacket}[av_packet_alloc()]
+    apPacket == [C_NULL] && error("av_packet_alloc() error")
 
     if eltype(firstimg) == UInt8
-        pix_fmt = VideoIO.AV_PIX_FMT_GRAY8
+        pix_fmt = AV_PIX_FMT_GRAY8
     elseif eltype(firstimg) == Gray{N0f8}
-            pix_fmt = VideoIO.AV_PIX_FMT_GRAY8
+            pix_fmt = AV_PIX_FMT_GRAY8
     elseif eltype(firstimg) == RGB{N0f8} && (codec_name == "libx264rgb")
         if !islossless(AVCodecContextProperties)
             @warn """Codec libx264rgb has limited playback support. Given that
             selected encoding settings are not lossless (crf!=0), codec_name="libx264"
             will give better results"""
         end
-        pix_fmt = VideoIO.AV_PIX_FMT_RGB24
+        pix_fmt = AV_PIX_FMT_RGB24
     elseif eltype(firstimg) == RGB{N0f8} && (codec_name == "libx264")
         if islossless(AVCodecContextProperties)
             @warn """Lossless encoding is selected (crf=0), however libx264 does
             not support lossless RGB planes. RGB will be downsampled to lossy YUV420P.
             To encode lossless RGB use codec_name="libx264rgb" """
         end
-        pix_fmt = VideoIO.AV_PIX_FMT_YUV420P
+        pix_fmt = AV_PIX_FMT_YUV420P
     else
         error("VideoIO: Encoding image element type $(eltype(imgstack[1])) with
         codec $codec_name not currently supported")
     end
 
-    codecContext = unsafe_load(codeccontextptr[1])
+    codecContext = unsafe_load(apCodecContext[1])
     codecContext.width = width
     codecContext.height = height
     codecContext.time_base = VideoIO.AVRational(1, framerate) # frames per second
     codecContext.framerate = VideoIO.AVRational(framerate, 1)
     codecContext.pix_fmt = pix_fmt
-    unsafe_store!(codeccontextptr[1], codecContext)
+    unsafe_store!(apCodecContext[1], codecContext)
 
-    codec_loaded = unsafe_load(codec)
+    codecContext = unsafe_load(apCodecContext[1])
     for prop in AVCodecContextProperties
         if prop[1] == :priv_data
             for pd in prop[2]
-                VideoIO.av_opt_set(codecContext.priv_data, string(pd[1]), string(pd[2]), VideoIO.AV_OPT_SEARCH_CHILDREN)
+                av_opt_set(codecContext.priv_data, string(pd[1]), string(pd[2]), AV_OPT_SEARCH_CHILDREN)
             end
         else
             setproperty!(codecContext, prop[1], prop[2])
@@ -116,29 +116,25 @@ function prepareencoder(firstimg,codec_name,framerate,AVCodecContextProperties)
     end
 
     # open it
-    ret = VideoIO.avcodec_open2(codeccontextptr[1], codec, C_NULL)
+    ret = avcodec_open2(apCodecContext[1], codec, C_NULL)
     ret < 0 && error("Could not open codec: Return code $(ret)")
 
-    frameptr = Ptr{VideoIO.AVFrame}[VideoIO.av_frame_alloc()]
-    frameptr == [C_NULL] && error("Could not allocate video frame")
-    frame = unsafe_load(frameptr[1])
+    apFrame = Ptr{VideoIO.AVFrame}[VideoIO.av_frame_alloc()]
+    apFrame == [C_NULL] && error("Could not allocate video frame")
+    frame = unsafe_load(apFrame[1])
     frame.format = codecContext.pix_fmt
     frame.width  = codecContext.width
     frame.height = codecContext.height
-    unsafe_store!(frameptr[1],frame)
+    unsafe_store!(apFrame[1],frame)
 
-    ret = VideoIO.av_frame_get_buffer(frameptr[1], 32)
+    ret = av_frame_get_buffer(apFrame[1], 32)
     ret < 0 && error("Could not allocate the video frame data")
 
-    return VideoEncoder(codec_name, codeccontextptr, frameptr, pktptr, pix_fmt)
+    return VideoEncoder(codec_name, apCodecContext, apFrame, apPacket, pix_fmt)
 end
 
 """
-appendencode(io::IO, img, index::Integer,
-    codeccontextptr::Array{Ptr{VideoIO.AVCodecContext}},
-    frameptr::Array{Ptr{VideoIO.AVFrame}},
-    pktptr::Array{Ptr{VideoIO.AVPacket}},
-    pix_fmt::Integer)
+appendencode(encoder::VideoEncoder, io::IO, img, index::Integer)
 
 Send image object to ffmpeg encoder and encode
 """
@@ -146,27 +142,26 @@ function appendencode!(encoder::VideoEncoder, io::IO, img, index::Integer)
 
     flush(stdout)
 
-    ret = VideoIO.av_frame_make_writable(encoder.pframe[1])
+    ret = av_frame_make_writable(encoder.apFrame[1])
     ret < 0 && error("av_frame_make_writable() error")
 
-    frame = unsafe_load(encoder.pframe[1]) #grab data from c memory
+    frame = unsafe_load(encoder.apFrame[1]) #grab data from c memory
 
     img_eltype = eltype(img)
 
-    if (img_eltype == UInt8) && (encoder.pix_fmt == VideoIO.AV_PIX_FMT_GRAY8)
-        framedata_1 = unsafe_load(frame.data[1])
+    if (img_eltype == UInt8) && (encoder.pix_fmt == AV_PIX_FMT_GRAY8)
         for y = 1:frame.height, x = 1:frame.width
             unsafe_store!(frame.data[1], img[x,y], ((y-1)*frame.linesize[1])+x)
         end
-    elseif (img_eltype  == Gray{N0f8}) && (encoder.pix_fmt == VideoIO.AV_PIX_FMT_GRAY8)
+    elseif (img_eltype  == Gray{N0f8}) && (encoder.pix_fmt == AV_PIX_FMT_GRAY8)
         img_uint8 = reinterpret(UInt8,img)
         for y = 1:frame.height, x = 1:frame.width
             unsafe_store!(frame.data[1], img_uint8[x,y], ((y-1)*frame.linesize[1])+x)
         end
-    elseif (img_eltype  == RGB{N0f8}) && (encoder.pix_fmt == VideoIO.AV_PIX_FMT_RGB24)
+    elseif (img_eltype  == RGB{N0f8}) && (encoder.pix_fmt == AV_PIX_FMT_RGB24)
         img_uint8 = reinterpret(UInt8, img)
         unsafe_copyto!(frame.data[1], pointer(img_uint8), length(img_uint8))
-    elseif (img_eltype  == RGB{N0f8}) && (encoder.pix_fmt == VideoIO.AV_PIX_FMT_YUV420P)
+    elseif (img_eltype  == RGB{N0f8}) && (encoder.pix_fmt == AV_PIX_FMT_YUV420P)
         img_YCbCr = convert.(YCbCr{Float64}, img)
         img_YCbCr_half = convert.(YCbCr{Float64}, restrict(img))
         for y = 1:frame.height, x = 1:frame.width
@@ -180,21 +175,21 @@ function appendencode!(encoder::VideoEncoder, io::IO, img, index::Integer)
         error("Array element type not supported")
     end
     frame.pts = index
-    unsafe_store!(encoder.pframe[1], frame) #pass data back to c memory
+    unsafe_store!(encoder.apFrame[1], frame) #pass data back to c memory
     encode!(encoder, io)
 end
 
 """
-function finishencode(io, codeccontextptr, frameptr, pktptr)
+function finishencode(encoder::VideoEncoder, io::IO)
 
 End encoding by sending endencode package to ffmpeg, and close objects.
 """
-function finishencode!(encoder::VideoEncoder, io)
+function finishencode!(encoder::VideoEncoder, io::IO)
     encode!(encoder, io, flush=true) # flush the encoder
     write(io,UInt8[0, 0, 1, 0xb7]) # add sequence end code to have a real MPEG file
-    VideoIO.avcodec_free_context(encoder.pcodeccontext)
-    VideoIO.av_frame_free(encoder.pframe)
-    VideoIO.av_packet_free(encoder.ppkt)
+    avcodec_free_context(encoder.apCodecContext)
+    av_frame_free(encoder.apFrame)
+    av_packet_free(encoder.apPacket)
 end
 
 """
@@ -203,7 +198,7 @@ mux(srcfilename,destfilename,framerate)
 Multiplex stream object into video container.
 """
 function mux(srcfilename,destfilename,framerate)
-    muxout = VideoIO.collectexecoutput(`$(VideoIO.ffmpeg) -y -framerate $framerate -i $srcfilename -c copy $destfilename`)
+    muxout = collectexecoutput(`$(ffmpeg) -y -framerate $framerate -i $srcfilename -c copy $destfilename`)
     filter!(x->!occursin.("Timestamps are unset in a packet for stream 0.",x),muxout)
     if occursin("ffmpeg version ",muxout[1]) && occursin("video:",muxout[end])
         rm("$srcfilename")
