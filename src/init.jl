b349ccb6d3e9d8bf070caa8d50e374579af16cb0
Ian <i.r.butterworth@gmail.com>
2019-07-12 14:40:27 -0400

    many changes

--- src/init.jl
+++ src/init.jl
@@ -1,14 +1,14 @@
-## convenient way to get library information
-using BinaryProvider
 
-# Load in `deps.jl`, complaining if it does not exist
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    println("Deps path: $depsjl_path")
-    error("VideoIO not installed properly, run `] build VideoIO`, restart Julia and try again")
-end
-include(depsjl_path)
+using FFMPEG
 
-include("version.jl")
+const av_load_path = joinpath(dirname(@__FILE__), "ffmpeg")
 
-av_load_path = joinpath(dirname(@__FILE__), "ffmpeg")
+const avcodec_dir    = joinpath(av_load_path, "AVCodecs",   "v$(FFMPEG._avcodec_version().major)")
+const avformat_dir   = joinpath(av_load_path, "AVFormat",   "v$(FFMPEG._avformat_version().major)")
+const avutil_dir     = joinpath(av_load_path, "AVUtil",     "v$(FFMPEG._avutil_version().major)")
+const swscale_dir    = joinpath(av_load_path, "SWScale",    "v$(FFMPEG._swscale_version().major)")
+const avdevice_dir   = joinpath(av_load_path, "AVDevice",   "v$(FFMPEG._avdevice_version().major)")
+const avfilter_dir   = joinpath(av_load_path, "AVFilters",  "v$(FFMPEG._avfilter_version().major)")
+#const avresample_dir = joinpath(av_load_path, "AVResample", "v$(_avresample_version().major)")
+#const swresample_dir = joinpath(av_load_path, "SWResample", "v$(_swresample_version().major)")
+
