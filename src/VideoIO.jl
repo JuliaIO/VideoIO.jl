5e7a95c90d9d382ab1524feca5248c183d2c3253
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-07-18 23:25:44 +1000

    Use ImageTransformations for restrict function

--- src/VideoIO.jl
+++ src/VideoIO.jl
@@ -4,6 +4,7 @@ using Libdl                                             #0.227492 seconds
 using Requires, Dates, ProgressMeter                    #0.001749 seconds
 using ImageCore: permuteddimsview, channelview, rawview #0.723065 seconds
 using ColorTypes: RGB, Gray, N0f8, YCbCr                #? seconds
+using ImageTransformations
 
 include("init.jl")
 include("util.jl")
