ed385052bc583f764a606d7b202fe92bffd60763
yiyuezhuo <yiyuezhuo@gmail.com>
2020-06-20 08:59:11 +0800

    skip memory leak test except linux

--- test/avio.jl
+++ test/avio.jl
@@ -263,47 +263,53 @@ end
 end
 
 @testset "c api memory leak test" begin
-    file = joinpath(videodir, "annie_oakley.ogg")
 
-    @testset "open file test" begin
-        check_size = 10
-        usage_vec = Vector{String}(undef, check_size)
+    if(Sys.islinux())  # TODO: find a method to get cross platform memory usage, see: https://discourse.julialang.org/t/how-to-get-current-julia-process-memory-usage/41734/4
+    
+        function get_memory_usage()
+            open("/proc/$(getpid())/statm") do io
+                split(read(io, String))[1]
+            end            
+        end
+    
+        file = joinpath(videodir, "annie_oakley.ogg")
 
-        for i in 1:check_size
-        
-            f = VideoIO.openvideo(file)
-            close(f)
-            GC.gc()
+        @testset "open file test" begin
+            check_size = 10
+            usage_vec = Vector{String}(undef, check_size)
 
-            out = read(`top -bn1 -p $(getpid())`, String)
-            usage = split(split(out,  "\n")[end-1])[6]
-            usage_vec[i] = usage
+            for i in 1:check_size
+            
+                f = VideoIO.openvideo(file)
+                close(f)
+                GC.gc()
+
+                usage_vec[i] = get_memory_usage()
+            end
+
+            println(usage_vec)
+
+            @test usage_vec[end-1] == usage_vec[end]
         end
 
-        println(usage_vec)
+        @testset "open and read file test" begin
+            check_size = 10
+            usage_vec = Vector{String}(undef, check_size)
 
-        @test usage_vec[end-1] == usage_vec[end]
-    end
+            for i in 1:check_size
+            
+                f = VideoIO.openvideo(file)
+                img = read(f)
+                close(f)
+                GC.gc()
 
-    @testset "open and read file test" begin
-        check_size = 10
-        usage_vec = Vector{String}(undef, check_size)
+                usage_vec[i] = get_memory_usage()
+            end
 
-        for i in 1:check_size
-        
-            f = VideoIO.openvideo(file)
-            img = read(f)
-            close(f)
-            GC.gc()
+            println(usage_vec)
 
-            out = read(`top -bn1 -p $(getpid())`, String)
-            usage = split(split(out,  "\n")[end-1])[6]
-            usage_vec[i] = usage
+            @test usage_vec[end-1] == usage_vec[end]
         end
-
-        println(usage_vec)
-
-        @test usage_vec[end-1] == usage_vec[end]
     end
 end
 
