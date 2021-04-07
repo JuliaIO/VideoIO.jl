dc902b26c6dfee6acb61c878e1736894067934f7
Ian <i.r.butterworth@gmail.com>
2021-04-07 11:18:59 -0400

    clearer debug

--- src/avio.jl
+++ src/avio.jl
@@ -305,12 +305,12 @@ function VideoReader(avin::AVInput{I}, video_stream = 1;
         dst_pix_fmt in VIO_GRAY_SCALE_TYPES &&
         colorspace_details.color_range != codec_context.color_range
 
-    @debug use_vio_gray_transform
-    @debug transcode
-    @debug codec_context.pix_fmt dst_pix_fmt (codec_context.pix_fmt == dst_pix_fmt)
-    @debug allow_vio_gray_transform
-    @debug (dst_pix_fmt in VIO_GRAY_SCALE_TYPES)
-    @debug colorspace_details.color_range codec_context.color_range (colorspace_details.color_range != codec_context.color_range)
+    @debug "use_vio_gray_transform" use_vio_gray_transform
+    @debug "transcode" transcode
+    @debug "pixel format" codec_context.pix_fmt dst_pix_fmt (codec_context.pix_fmt == dst_pix_fmt)
+    @debug "allow transform" allow_vio_gray_transform
+    @debug "allowed pix format" (dst_pix_fmt in VIO_GRAY_SCALE_TYPES)
+    @debug "color_range" colorspace_details.color_range codec_context.color_range (colorspace_details.color_range != codec_context.color_range)
 
     if use_vio_gray_transform
         frame_graph = GrayTransform()
