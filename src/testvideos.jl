bcacac293a72c602ee8eb9ffc7d047f5f27cd68b
Ian Butterworth <i.r.butterworth@gmail.com>
2023-11-28 14:41:13 -0500

    remove videos dir entirely

--- src/testvideos.jl
+++ src/testvideos.jl
@@ -1,5 +1,14 @@
-# Tools for downloading test videos
+"""
+    VideoIO.TestVideos
 
+Tools for downloading test videos
+
+* `VideoIO.TestVideos.available()` prints a list of all available test videos.
+* `VideoIO.testvideo("annie_oakley")` returns an AVInput object for the
+  "annie_oakley" video. The video will be downloaded if it isn't available.
+* `VideoIO.TestVideos.download_all()` downloads all test videos
+* `VideoIO.TestVideos.remove_all()` removes all test videos
+"""
 module TestVideos
 
 using VideoIO: VideoIO
